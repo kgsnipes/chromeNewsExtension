c9f23713e21e49acd454d92a7604b5d704085423
kaushik ganguly <kgsnipes@gmail.com>
2013-10-17 00:54:02 +0530

    design

--- js/kgnewssetup.js
+++ js/kgnewssetup.js
@@ -18,6 +18,24 @@ function ndtv_rss_parser(url)
 
 (function init()
 {
+
 	getXMLData("http://www.techmeme.com/feed.xml",function(xml){console.log(getNewsItems(xml));},function(){console.log("failed");});
 })();
 
+$(document).ready(function(){
+
+
+	$(".closeBtn").hover(function(){$(this).animate({marginLeft:'-80px'},200);}).mouseout(function(){$(this).animate({marginLeft:'-25px'},200);}).click(function(){window.close();});
+
+
+	populateChannels();
+
+});
+
+
+function populateChannels()
+{
+	$.each(kgchannels,function(){
+		$(".channels ul.channelList").append("<li class=\"channelItem channelItemBorder\">"+this.name+"</li>");
+	});
+}
